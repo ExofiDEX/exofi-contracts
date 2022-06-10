9d777c82c394dc27354c6a2a7188e77276b1ff0a
Ing. Michael Goldfinger <106750422+migoldfinger@users.noreply.github.com>
2022-06-10 14:29:06 +0200

    fix: unsolvable inheritance

--- contracts/Fermion.sol
+++ contracts/Fermion.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@exoda/exoda-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@exoda/exoda-contracts/contracts/access/Ownable.sol";
+import "@exoda/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@exoda/contracts/access/Ownable.sol";
 import "./interfaces/IFermion.sol";
 
 /**
@@ -10,22 +10,10 @@ import "./interfaces/IFermion.sol";
 * The IFermion interface extends the IERC20, IERC20Metadata, IERC20Burnable, IOwnable interfaces.
 *
 */
-contract Fermion is ERC20Burnable, Ownable, IFermion
+contract Fermion is IFermion, ERC20Burnable, Ownable
 {
 	// solhint-disable-next-line no-empty-blocks
-	constructor() ERC20("Fermion", "EXOFI") {}
-
-	/// @notice Destroys `amount` tokens from the caller.
-	function burn(uint256 amount) public override (ERC20Burnable, IFermion)
-	{
-		ERC20Burnable.burn(amount);
-	}
-
-	/// @notice Destroys `amount` tokens from `account`, deducting from the caller's allowance.
-	function burnFrom(address account, uint256 amount) public override(ERC20Burnable, IFermion)
-	{
-		ERC20Burnable.burnFrom(account, amount);
-	}
+	constructor() ERC20Burnable("Fermion", "EXOFI") {}
 
 	/// @notice Creates `amount` token to `to`. Must only be called by the owner (MagneticFieldGenerator).
 	function mint(address to, uint256 amount) public override onlyOwner
