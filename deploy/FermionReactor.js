71e591188e21505db18388bb9375bee52bb0df09
Ing. Michael Goldfinger <106750422+migoldfinger@users.noreply.github.com>
2022-10-24 17:31:20 +0200

    feat: deployment script

---
+++ deploy/FermionReactor.js
@@ -0,0 +1,20 @@
+module.exports = async function ({ ethers, getNamedAccounts, deployments })
+{
+	const { deploy } = deployments;
+	const { deployer } = await getNamedAccounts();
+
+	console.log("FermionReactor - Deploying contracts with deployer: ", deployer);
+
+	const fermion = await ethers.getContract("Fermion");
+
+	await deploy("FermionReactor",
+		{
+			from: deployer,
+			args: ["100000000000000000", "3000000000000000000", fermion.address, "12000"],
+			log: true,
+			deterministicDeployment: false
+		});
+};
+
+module.exports.tags = ["FermionReactor"];
+module.exports.dependencies = ["Fermion"];
